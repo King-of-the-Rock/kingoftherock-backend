d408d4bf1822b5dabfcc7be0dfb5ea0a2148b095
Matthew Renze <merenze@gmail.com>
2022-05-18 18:07:50 -0500

    Validate user login info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class UserController
 {
+	/**
+	 * @param Request $request
+	 *
+	 * @return JsonResponse
+	 */
+	public function register(Request $request): JsonResponse
+	{
+		$validator = Validator::make($request->all(), [
+			'email' => [
+				'required',
+				'email',
+				'unique:users',
+			],
+			'username' => [
+				'required',
+				'unique:users',
+			],
+			'password' => [
+				'required',
+			],
+		]);
+		
+		if ($validator->fails()) {
+			return response()->json($validator->errors())->setStatusCode(400);
+		}
+		
+		return response()->json($request->all());
+	}
 }
