c7d457381fd6b5fb31882d8688ebcfdca7e00902
Matthew Renze <merenze@gmail.com>
2022-05-18 23:12:37 -0500

    User indexing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
 class UserController extends Controller
 {
-	public function index() {
-
+	/**
+	 * @return JsonResponse
+	 */
+	public function index(): JsonResponse
+	{
+		return response()->json(User::all(['id', 'username']));
 	}
 
-	public function show(User $user) {
-
+	public function show(User $user)
+	{
 	}
 
-	public function update(Request $request, User $user) {
-
+	public function update(Request $request, User $user)
+	{
 	}
 
-	public function delete(User $user) {
-
+	public function delete(User $user)
+	{
 	}
 }
